88273870164338ab86fdbce539ae7005c1f82cbe
grau-maus <joshtupas@gmail.com>
2022-04-11 17:35:55 -0600

    add bootstrap framework library. clean up unnecessary code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import SignupForm from "./components/SignupForm";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
   return (
     <>
-      <Navbar />
-      {isLoaded && (
-        <Switch>
-          <Route path="/signup" component={SignupForm} />
-        </Switch>
-      )}
-      <Footer />
+      <SignupForm />
     </>
   );
 }
