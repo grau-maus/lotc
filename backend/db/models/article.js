39fb1704954d85b182a8a61fd6cb1680f1dd1e1a
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2022-04-10 20:39:21 -0500

    first take at db

---
+++ backend/db/models/article.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Article = sequelize.define('Article', {
+    text: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    userId: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+    }
+  }, {});
+  Article.associate = function(models) {
+    Article.belongsTo(models.User, { foreignKey: 'userId'});
+    Article.hasMany(models.Comment, { foreignKey:commentId, onDelete:'CASCADE' })
+  };
+  return Article;
+};
